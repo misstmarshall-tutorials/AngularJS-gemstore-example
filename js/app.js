b212bf0b7cc8c3c97ae5a5faeb7730eea5d34fdc
Taneshia Marshall <taneshia.marshall@gmail.com>
2015-09-14 22:27:19 -0400

    Added a controller that will control the behavior of the application

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+(function(){
 //a module line that declares the "app"lication by calling the module function in the angular js library and naming it "store"
 //include an empty array since there are no dependencies
-var app = angular.module('store', []);
+var app = angular.module("store", []);
+
+app.controller("StoreController", function(){
+	this.product = gem;
+});
+
+var gem = {
+	name: "Dodecahedron",
+	price:2.95,
+	description: "..."
+}
+})();
