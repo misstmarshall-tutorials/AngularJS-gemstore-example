00f6cf8c33331a73a7a883a7533993fec0c30970
Taneshia Marshall <taneshia.marshall@gmail.com>
2015-09-14 22:16:33 -0400

    Linked the libraries to the webpage and declared the app as an angular application so that angular directives can be recognized

---
+++ js/app.js
@@ -0,0 +1,3 @@
+//a module line that declares the "app"lication by calling the module function in the angular js library and naming it "store"
+//include an empty array since there are no dependencies
+var app = angular.module('store', []);
