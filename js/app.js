7db7e775641828c5fd04ed7c18cac5e773334156
Taneshia Marshall <taneshia.marshall@gmail.com>
2015-09-15 00:16:47 -0400

    Displayed the first image of the array for each gem, couldn't get the thumbnail images to show

--- js/app.js
+++ js/app.js
@@ -10,37 +10,67 @@ var gems = [
 		name: "1 Garnet Gem",
 		price:2.95,
 		description: "Information about the Garnet Gem goes here...",
+		images: [
+			"img/1.png",
+			"img/1.png",
+			"img/1.png"
+		],
 		canPurchase: true
 	},
 	{
 		name: "2 Amethyst Gem",
 		price:5.95,
 		description: "Information about the Amethyst Gem goes here...",
+		images: [
+			"img/2.png",
+			"img/2.png",
+			"img/2.png"
+		],
 		canPurchase: true
 	},
 	{ 
 		name: "3 Aquamarine Gem", 
 		price: 2.95,
 		description: "Information about the Aquamarine Gem goes here...",
-		canPurchase: true,
+		images: [
+			"img/3.png",
+			"img/3.png",
+			"img/3.png"
+		],
+		canPurchase: true
 	},
-    { 
+   { 
     	name: "4 Diamond Gem", 
 	    price: 5.95,
 	    description: "Information about the Diamond Gem goes here...",
-		canPurchase: true,
+	    images: [
+			"img/4.png",
+			"img/4.png",
+			"img/4.png"
+		],
+		canPurchase: true
 	},
     { 
     	name: "5 Emerald Gem", 
 	    price: 3.95,
 	    description: "Information about the Emerald Gem goes here...",
-		canPurchase: true,
+	    images: [
+			"img/5.png",
+			"img/5.png",
+			"img/5.png"
+		],
+		canPurchase: true
 	},
 	 { 
     	name: "6 Pearl Gem", 
 	    price: 5.95,
 	    description: "Information about the Pearl Gem goes here...",
-		canPurchase: true,
+	   images: [
+			"img/6.png",
+			"img/6.png",
+			"img/6.png"
+		],
+		canPurchase: true
 	}
 ];
 })();
