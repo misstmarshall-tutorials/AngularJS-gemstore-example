5ffec562fa481e45928ec84d875eb02e32d3780d
Taneshia Marshall <taneshia.marshall@gmail.com>
2015-09-14 22:43:04 -0400

    Created more gems to display and iterated through the array of gems to make it more dynamic when displaying their information

--- js/app.js
+++ js/app.js
@@ -2,14 +2,36 @@
 var app = angular.module("store", []);
 
 app.controller("StoreController", function(){
-	this.product = gem;
+	this.products = gems;
 });
 
-var gem = {
-	name: "Dodecahedron",
-	price:2.95,
-	description: "...",
+var gems = [
+	{
+		name: "Dodecahedron Gem",
+		price:2.95,
+		description: "Information about the Dodecahedron Gem goes here...",
+		canPurchase: true
+	},
+	{
+		name: "Pentagonal Gem",
+		price:5.95,
+		description: "Information about the Pentagonal Gem goes here...",
+		canPurchase: true
+	},
+	{ name: 'Azurite Gem', 
+	price: 2.95,
+	description: "Information about the Azurite Gem goes here...",
 	canPurchase: true,
-	soldOut:false
-}
+	},
+    { name: 'Bloodstone Gem', 
+    price: 5.95,
+    description: "Information about the Bloodstone Gem goes here...",
+	canPurchase: true,
+	},
+    { name: 'Zircon', 
+    price: 3.95,
+    description: "Information about the Zircon Gem goes here...",
+	canPurchase: true,
+	}
+];
 })();
