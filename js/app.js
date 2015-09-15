a304ebc0db1645b5f749e28a660d45ccb9fee506
Taneshia Marshall <taneshia.marshall@gmail.com>
2015-09-14 23:38:49 -0400

    Fixed the issue where the "add to cart" button wasn't showing, used the currency filter to auto format the price, added images to the img directory

--- js/app.js
+++ js/app.js
@@ -7,31 +7,40 @@ app.controller("StoreController", function(){
 
 var gems = [
 	{
-		name: "Dodecahedron Gem",
+		name: "1 Garnet Gem",
 		price:2.95,
-		description: "Information about the Dodecahedron Gem goes here...",
+		description: "Information about the Garnet Gem goes here...",
 		canPurchase: true
 	},
 	{
-		name: "Pentagonal Gem",
+		name: "2 Amethyst Gem",
 		price:5.95,
-		description: "Information about the Pentagonal Gem goes here...",
+		description: "Information about the Amethyst Gem goes here...",
 		canPurchase: true
 	},
-	{ name: 'Azurite Gem', 
-	price: 2.95,
-	description: "Information about the Azurite Gem goes here...",
-	canPurchase: true,
+	{ 
+		name: "3 Aquamarine Gem", 
+		price: 2.95,
+		description: "Information about the Aquamarine Gem goes here...",
+		canPurchase: true,
 	},
-    { name: 'Bloodstone Gem', 
-    price: 5.95,
-    description: "Information about the Bloodstone Gem goes here...",
-	canPurchase: true,
+    { 
+    	name: "4 Diamond Gem", 
+	    price: 5.95,
+	    description: "Information about the Diamond Gem goes here...",
+		canPurchase: true,
 	},
-    { name: 'Zircon', 
-    price: 3.95,
-    description: "Information about the Zircon Gem goes here...",
-	canPurchase: true,
+    { 
+    	name: "5 Emerald Gem", 
+	    price: 3.95,
+	    description: "Information about the Emerald Gem goes here...",
+		canPurchase: true,
+	},
+	 { 
+    	name: "6 Pearl Gem", 
+	    price: 5.95,
+	    description: "Information about the Pearl Gem goes here...",
+		canPurchase: true,
 	}
 ];
 })();
