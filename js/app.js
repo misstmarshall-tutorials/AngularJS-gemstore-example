d26c236b2a1bc36348544f3e185a31a5e7abe1d6
Taneshia Marshall <taneshia.marshall@gmail.com>
2015-09-18 18:59:16 -0400

    Added reviews for each product

--- js/app.js
+++ js/app.js
@@ -5,76 +5,7 @@ app.controller("StoreController", function(){
 	this.products = gems;
 });
 
-var gems = [
-	{
-		name: "1 Garnet Gem",
-		price:2.95,
-		description: "Information about the Garnet Gem goes here...",
-		images: [
-			"img/1.png",
-			"img/1.png",
-			"img/1.png"
-		],
-		canPurchase: true
-	},
-	{
-		name: "2 Amethyst Gem",
-		price:5.95,
-		description: "Information about the Amethyst Gem goes here...",
-		images: [
-			"img/2.png",
-			"img/2.png",
-			"img/2.png"
-		],
-		canPurchase: true
-	},
-	{ 
-		name: "3 Aquamarine Gem", 
-		price: 2.95,
-		description: "Information about the Aquamarine Gem goes here...",
-		images: [
-			"img/3.png",
-			"img/3.png",
-			"img/3.png"
-		],
-		canPurchase: true
-	},
-   { 
-    	name: "4 Diamond Gem", 
-	    price: 5.95,
-	    description: "Information about the Diamond Gem goes here...",
-	    images: [
-			"img/4.png",
-			"img/4.png",
-			"img/4.png"
-		],
-		canPurchase: true
-	},
-    { 
-    	name: "5 Emerald Gem", 
-	    price: 3.95,
-	    description: "Information about the Emerald Gem goes here...",
-	    images: [
-			"img/5.png",
-			"img/5.png",
-			"img/5.png"
-		],
-		canPurchase: true
-	},
-	 { 
-    	name: "6 Pearl Gem", 
-	    price: 5.95,
-	    description: "Information about the Pearl Gem goes here...",
-	   images: [
-			"img/6.png",
-			"img/6.png",
-			"img/6.png"
-		],
-		canPurchase: true
-	}
-];
-
-	app.controller("PanelController", function(){
+app.controller("PanelController", function(){
 	this.tab = 1;
 
 	this.selectTab = function(setTab){
@@ -86,4 +17,144 @@ var gems = [
 	}
 });
 
+var gems = [
+	{
+		name: "1 Garnet Gem",
+		price:2.95,
+		description: "Information about the Garnet Gem goes here...",
+		images: [
+			"img/1.png",
+			"img/1.png",
+			"img/1.png"
+		],
+		canPurchase: true,
+		reviews:[
+		{
+			stars:5,
+			body: "I love this product!",
+			author: "joe@thomas.com"
+		},
+		{
+			stars:1,
+			body: "This product sucks",
+			author: "tim@hater.com"
+		}
+		]
+	},
+	{
+		name: "2 Amethyst Gem",
+		price:5.95,
+		description: "Information about the Amethyst Gem goes here...",
+		images: [
+			"img/2.png",
+			"img/2.png",
+			"img/2.png"
+		],
+		canPurchase: true,
+		reviews:[
+		{
+			stars:5,
+			body: "I love this product!",
+			author: "joe@thomas.com"
+		},
+		{
+			stars:1,
+			body: "This product sucks",
+			author: "tim@hater.com"
+		}
+		]
+	},
+	{ 
+		name: "3 Aquamarine Gem", 
+		price: 2.95,
+		description: "Information about the Aquamarine Gem goes here...",
+		images: [
+			"img/3.png",
+			"img/3.png",
+			"img/3.png"
+		],
+		canPurchase: true,
+		reviews:[
+		{
+			stars:5,
+			body: "I love this product!",
+			author: "joe@thomas.com"
+		},
+		{
+			stars:1,
+			body: "This product sucks",
+			author: "tim@hater.com"
+		}
+		]
+	},
+   { 
+    	name: "4 Diamond Gem", 
+	    price: 5.95,
+	    description: "Information about the Diamond Gem goes here...",
+	    images: [
+			"img/4.png",
+			"img/4.png",
+			"img/4.png"
+		],
+		canPurchase: true,
+		reviews:[
+		{
+			stars:5,
+			body: "I love this product!",
+			author: "joe@thomas.com"
+		},
+		{
+			stars:1,
+			body: "This product sucks",
+			author: "tim@hater.com"
+		}
+		]
+	},
+    { 
+    	name: "5 Emerald Gem", 
+	    price: 3.95,
+	    description: "Information about the Emerald Gem goes here...",
+	    images: [
+			"img/5.png",
+			"img/5.png",
+			"img/5.png"
+		],
+		canPurchase: true,
+		reviews:[
+		{
+			stars:5,
+			body: "I love this product!",
+			author: "joe@thomas.com"
+		},
+		{
+			stars:1,
+			body: "This product sucks",
+			author: "tim@hater.com"
+		}
+		]
+	},
+	 { 
+    	name: "6 Pearl Gem", 
+	    price: 5.95,
+	    description: "Information about the Pearl Gem goes here...",
+	   images: [
+			"img/6.png",
+			"img/6.png",
+			"img/6.png"
+		],
+		canPurchase: true,
+		reviews:[
+		{
+			stars:5,
+			body: "I love this product!",
+			author: "joe@thomas.com"
+		},
+		{
+			stars:1,
+			body: "This product sucks",
+			author: "tim@hater.com"
+		}
+		]
+	}
+];
 })();
