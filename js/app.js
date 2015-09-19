0545b7405d75f90833910d4f1e8cdcc73a7c12c1
Taneshia Marshall <taneshia.marshall@gmail.com>
2015-09-18 20:08:41 -0400

    Added title, broken image gallery, comments, and live review form

--- js/app.js
+++ js/app.js
@@ -8,13 +8,20 @@ app.controller("StoreController", function(){
 app.controller("PanelController", function(){
 	this.tab = 1;
 
-	this.selectTab = function(setTab){
-		this.tab = setTab;
+	this.selectTab = function(newValue){
+		this.tab = newValue;
 	}
 
-	this.isSelected = function(checkTab){
-		return this.tab === checkTab;
+	this.isSelected = function(tabName){
+		return this.tab === tabName;
 	}
+});
+
+app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    };
 });
 
 var gems = [
@@ -22,10 +29,11 @@ var gems = [
 		name: "1 Garnet Gem",
 		price:2.95,
 		description: "Information about the Garnet Gem goes here...",
+		shine: 8,
 		images: [
 			"img/1.png",
-			"img/1.png",
-			"img/1.png"
+			"img/2.png",
+			"img/3.png"
 		],
 		canPurchase: true,
 		reviews:[
@@ -45,10 +53,11 @@ var gems = [
 		name: "2 Amethyst Gem",
 		price:5.95,
 		description: "Information about the Amethyst Gem goes here...",
+		shine: 8,
 		images: [
 			"img/2.png",
-			"img/2.png",
-			"img/2.png"
+			"img/3.png",
+			"img/4.png"
 		],
 		canPurchase: true,
 		reviews:[
@@ -68,10 +77,11 @@ var gems = [
 		name: "3 Aquamarine Gem", 
 		price: 2.95,
 		description: "Information about the Aquamarine Gem goes here...",
+		shine: 8,
 		images: [
 			"img/3.png",
-			"img/3.png",
-			"img/3.png"
+			"img/4.png",
+			"img/5.png"
 		],
 		canPurchase: true,
 		reviews:[
@@ -91,10 +101,12 @@ var gems = [
     	name: "4 Diamond Gem", 
 	    price: 5.95,
 	    description: "Information about the Diamond Gem goes here...",
+	    shine: 8,
+	    shine: 8,
 	    images: [
 			"img/4.png",
-			"img/4.png",
-			"img/4.png"
+			"img/5.png",
+			"img/6.png"
 		],
 		canPurchase: true,
 		reviews:[
@@ -114,10 +126,11 @@ var gems = [
     	name: "5 Emerald Gem", 
 	    price: 3.95,
 	    description: "Information about the Emerald Gem goes here...",
+	    shine: 8,
 	    images: [
 			"img/5.png",
-			"img/5.png",
-			"img/5.png"
+			"img/6.png",
+			"img/7.png"
 		],
 		canPurchase: true,
 		reviews:[
@@ -137,10 +150,11 @@ var gems = [
     	name: "6 Pearl Gem", 
 	    price: 5.95,
 	    description: "Information about the Pearl Gem goes here...",
+	    shine: 8,
 	   images: [
 			"img/6.png",
-			"img/6.png",
-			"img/6.png"
+			"img/7.png",
+			"img/8.png"
 		],
 		canPurchase: true,
 		reviews:[
